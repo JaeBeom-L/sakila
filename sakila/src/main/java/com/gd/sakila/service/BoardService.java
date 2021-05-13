9618586fca519251c391d6bc8d1026e6564c91d3
JaeBeom-L <k2jb8180@gmail.com>
2021-05-13 17:19:19 +0900

    BoardList(페이징, 검색기능), BoardOne페이지 생성, Board 게시물 입력 기능 구현
    issue : Board 수정, 삭제 기능 구현 필요

---
+++ sakila/src/main/java/com/gd/sakila/service/BoardService.java
@@ -0,0 +1,60 @@
+package com.gd.sakila.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gd.sakila.mapper.BoardMapper;
+import com.gd.sakila.vo.Board;
+import com.gd.sakila.vo.Page;
+
+@Service
+@Transactional
+public class BoardService {
+	@Autowired
+	BoardMapper boardMapper;
+	
+	// board 등록 서비스
+	public int addBoard(Board board) {
+		return boardMapper.insertBoard(board);
+	}
+	
+	//boardOne 자세히보기 서비스
+	public Map<String, Object> getBoardOne(int boardId) {
+		return boardMapper.selectBoardOne(boardId);
+	}
+	
+	// boardList 출력 서비스
+	public Map<String, Object> getBoardList(int currentPage, int rowPerPage, String searchWord){
+		// 1. 
+		int boardTotal = boardMapper.selectBoardTotal(searchWord); // 총 게시물 수(searchWord)
+		int lastPage = boardTotal/rowPerPage;	// 마지막페이지
+		if(boardTotal % rowPerPage != 0) { // rowPerPage에 딱 나눠 떨어지지 않으면 마지막페이지에 1을 더한다.
+			lastPage ++;
+		}
+		
+		System.out.println(boardTotal+" BoardService-boardTotal"); //디버깅 코드
+		System.out.println(lastPage+" BoardService-lastPage"); //디버깅 코드
+		
+		// 2.
+		Page page = new Page(); // page 변수에 시작행, 찾는 단어, 보여줄 행의 수 탑재
+		page.setBeginRow((currentPage-1)*rowPerPage);
+		page.setSearchWord(searchWord);
+		page.setRowPerPage(rowPerPage);
+		
+		List<Board> boardList = boardMapper.selectBoardList(page);  // controller에서 받아온 매개변수로 boardList 생성
+	
+		Map<String, Object> map = new HashMap<>(); // Map 타입의 map에 컨트롤러에 필요한 데이터 저장
+		map.put("boardTotal", boardTotal);
+		map.put("lastPage", lastPage);
+		map.put("boardList", boardList);
+			
+		return map;
+	}
+	
+
+}	
